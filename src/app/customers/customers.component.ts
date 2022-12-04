92d64f12d7c116a8c37b300a494b27fbb57d9ba8
sanjayadayarathne <sanjayaruwan94@gmail.com>
2022-12-04 14:09:57 +0530

    feat: customer page

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-customers',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
-  constructor() { }
+  customerDocumentId: any = null
+  customers: Array<any> = new Array<any>()
 
-  ngOnInit(): void {
+  isSearched: boolean = false
+  searchedResults: Array<any> = new Array<any>()
+  searchedKeys: any
+
+  customersRef: AngularFirestoreCollection<any>;
+  
+  constructor(private router: Router, 
+              private angularFireStore: AngularFirestore) { 
+      
+      // query for retrieve only customer type users          
+      this.customersRef = this.angularFireStore.collection('users', ref => ref.where('type', '==', 'customer'));
   }
 
+  ngOnInit(): void {
+    this.retrieveCustomers()
+  }
+
+  navigateToCustomerProfile(customerDetails: any) {
+    sessionStorage.setItem('customerDetails', JSON.stringify(customerDetails))
+    this.router.navigateByUrl('/main/customer-profile')
+  }
+
+  retrieveCustomers(): void {
+    this.customersRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+        )
+      )
+    ).subscribe(data => {
+      console.log(data)
+      this.customers = data;
+    });
+  }
+
+  searchByCustomerNIC() {
+    console.log(this.searchedKeys)
+    if (this.searchedKeys.length > 0) {
+      this.isSearched = true
+      this.searchedResults = []
+      this.customers.forEach(customer => {
+        if (customer.nic.startsWith(this.searchedKeys.toUpperCase())) {
+          this.searchedResults.push(customer)
+        }
+      });
+    } else {
+      this.isSearched = false
+      this.searchedResults = []
+    }
+  }
 }
